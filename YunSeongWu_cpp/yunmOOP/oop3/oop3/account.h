380484c4910fc006c82d7b6089f31811088681fd
simjeehoon <simjeehoon@naver.com>
2017-10-08 17:52:46 +0900

    oop3

---
+++ YunSeongWu_cpp/yunmOOP/oop3/oop3/account.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __ACCOUNT_DEF__
+#define __ACCOUNT_DEF__
+
+class Account
+{
+private:
+	char * acc_id;
+	char * name;
+	int balance;
+	static int member_count;
+public:
+	Account(char * id, char * name, int deposit);
+	Account(Account & copy);
+	~Account();
+	char * GetAccID();
+	int GetBalance();
+	void Showdata();
+	int withdraw(int);
+	int deposit(int);
+	static int GetMemberCount();
+	static void AddMemberCount();
+};
+
+#endif
